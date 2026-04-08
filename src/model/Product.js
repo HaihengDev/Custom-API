0c82fe90e5734d4f30e6329c19f2efd7823a1d48
HaihengDev <haihenglim9@gmail.com>
2026-04-08 22:59:35 +0700

    Add product model

--- src/model/Product.js
+++ src/model/Product.js
@@ -1,2 +1,23 @@
 import mongoose from 'mongoose';
 
+const productSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  }
+});
+
+export default mongoose.models.Product || mongoose.model('Product', productSchema);
+
